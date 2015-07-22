8238d5f903af252611208f9bc1d10e3d3437e646
jeremybdavis <jdavis129@gmail.com>
2015-07-22 16:22:45 -0500

    Initial Markup

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 var mainUrl = 'https://api.etsy.com/v2/listings/active?api_key=h9oq2yf3twf4ziejn10b717i&keywords=guitar&includes=Images,Shop';
+
+$.ajax(mainUrl,{
+  error: function(){
+
+  },
+  success: function(data){
+    var content = data;
+  }
+})

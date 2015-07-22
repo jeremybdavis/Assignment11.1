616707fdba905b0ed8ff64316369084189a5e50c
jeremybdavis <jdavis129@gmail.com>
2015-07-22 17:18:39 -0500

    fixed api issue

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
-var mainUrl = 'https://api.etsy.com/v2/listings/active?api_key=h9oq2yf3twf4ziejn10b717i&keywords=guitar&includes=Images,Shop';
+var mainUrl = 'https://openapi.etsy.com/v2/listings/active.js?api_key=h9oq2yf3twf4ziejn10b717i&keywords=guitar&includes=Images,Shop';
 
-$.ajax(mainUrl,{
+$.ajax(mainUrl, {
+  dataType: 'jsonp',
   error: function(){
 
   },
   success: function(data){
     var content = data;
+    console.log(content);
   }
 })

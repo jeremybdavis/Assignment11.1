da283af2694dc621dbb064cdb08dee794ff172dd
jeremybdavis <jdavis129@gmail.com>
2015-07-23 17:35:49 -0500

    Template is working

--- js/main.js
+++ js/main.js
@@ -5,8 +5,45 @@ $.ajax(mainUrl, {
   error: function(){
 
   },
-  success: function(data){
-    var content = data;
-    console.log(content);
+  success: function(data, textStatus, xhr){
+    buildAllItems(data.results);
   }
 })
+
+var buildAllItems = function(items){
+  var html = items.map(buildItems);
+
+  html.reduce(function(html, item){
+    return html + item;
+  });
+
+  var $items = $('#primary-content');
+  $items.html(html);
+};
+
+var buildItems = function(item){
+  var image = _.first(item.Images);
+      image = image.url_170x135;
+
+
+  var imgUrl = item.url;
+  var title = item.title;
+  // var shop = item.shop.shop_name;
+
+  var itemTemplate = $('#itemTemplate').html();
+  var itemHTML = _.template(itemTemplate);
+  var output = itemHTML(
+    {
+      image: image,
+      imgUrl: imgUrl,
+      title: title,
+      // shop: shop,
+    }
+  );
+
+  return output;
+};
+
+// jQuery.ajax(mainUrl).done(function(items){
+//   buildAllRepos(items);
+// });
